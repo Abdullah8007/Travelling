9592b6dc1d1f788ee50f1a5a50a5852ad8d63d20
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-11-26 11:52:08 +0530

    ADD: Travelling

--- form_handler.php
+++ form_handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'code@programmer.com';
+$email_from = '?';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
